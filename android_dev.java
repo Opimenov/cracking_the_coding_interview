2cedfbeb6c96b0eb90303230577da39520948fb0
Oleksandr Pimenov <pialgi@live.com>
2017-04-19 09:08:40 -0400

    fixed the comments in map analizing tool

--- android_dev.java
+++ android_dev.java
@@ -1,7 +1,29 @@
 import java.lang.*;
 import java.util.*;
 /*
-Write the code to resolve this problem: You are given a square map of size. Each cell of the map has a value denoting its depth. We will call a cell of the map a cavity if and only if this cell is not on the border of the map and each cell adjacent to it has strictly smaller depth. Two cells are adjacent if they have a common side (edge). You need to find all the cavities on the map and depict them with the uppercase character X. Input FormatThe first line contains an integer, denoting the size of the map. Each of the following lines contains positive digits without spaces. Each digit (1-9) denotes the depth of the appropriate area. Constraints Output FormatOutput lines, denoting the resulting map. Each cavity should be replaced with character X. Sample Input 41112191218921234. Sample Output 11121X1218X21234. Explanation: The two cells with the depth of 9 fulfill all the conditions of the Cavity definition and have been replaced by X
+Write the code to resolve this problem:
+You are given a square map of size. Each cell of the
+map has a value denoting its depth. We will call a
+cell of the map a cavity if and only if this cell
+is not on the border of the map and each cell
+adjacent to it has strictly smaller depth. Two cells
+are adjacent if they have a common side (edge).
+You need to find all the cavities on the map and
+depict them with the uppercase character X. Input
+FormatThe first line contains an integer, denoting
+the size of the map. Each of the following lines
+contains positive digits without spaces. Each digit
+(1-9) denotes the depth of the appropriate area.
+Constraints Output FormatOutput lines, denoting the
+resulting map. Each cavity should be replaced with
+character X.
+
+Sample Input  41112191218921234.
+Sample Output 11121X1218X21234.
+
+Explanation: The two cells with the depth of 9
+fulfill all the conditions of the Cavity definition
+and have been replaced by X
  */
 public class android_dev{
     class Point{
